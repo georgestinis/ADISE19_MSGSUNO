5bdf18c9cdc9f722597194fd219de3a7d77ca914
MariosSkoufitsas <49339658+MariosSkoufitsas@users.noreply.github.com>
2019-12-08 21:28:59 +0200

    Update uno.sql

--- db/uno.sql
+++ db/uno.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Φιλοξενητής: 127.0.0.1
--- Χρόνος δημιουργίας: 06 Δεκ 2019 στις 14:47:13
+-- Χρόνος δημιουργίας: 08 Δεκ 2019 στις 20:28:09
 -- Έκδοση διακομιστή: 10.4.8-MariaDB
--- Έκδοση PHP: 7.3.11
+-- Έκδοση PHP: 7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -49,6 +49,14 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `draw_card` (IN `player_name` ENUM('
     SELECT card_id into c_id FROM remaining_deck ORDER BY RAND() LIMIT 1; 
     INSERT INTO hand VALUES (player_name, c_id);
     DELETE FROM remaining_deck WHERE card_id=c_id;
+END$$
+
+DROP PROCEDURE IF EXISTS `start_game`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `start_game` ()  BEGIN
+DECLARE c_code varchar(3);
+SELECT card_code into c_code FROM remaining_deck ORDER BY RAND() LIMIT 1;
+INSERT INTO table_deck(card_code) VALUES (c_code);
+DELETE FROM remaining_deck WHERE c_code = card_code;
 END$$
 
 DELIMITER ;
@@ -218,6 +226,26 @@ CREATE TABLE `hand` (
   `card_id` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Άδειασμα δεδομένων του πίνακα `hand`
+--
+
+INSERT INTO `hand` (`player_name`, `card_id`) VALUES
+('p1', 3),
+('p1', 10),
+('p2', 29),
+('p1', 39),
+('p1', 42),
+('p2', 44),
+('p2', 55),
+('p2', 60),
+('p1', 73),
+('p1', 82),
+('p2', 88),
+('p2', 98),
+('p1', 106),
+('p2', 108);
+
 -- --------------------------------------------------------
 
 --
@@ -235,7 +263,8 @@ CREATE TABLE `player` (
 --
 
 INSERT INTO `player` (`player_name`, `username`) VALUES
-('p1', 'georgestinis');
+('p1', 'georgestinis'),
+('p2', 'random');
 
 -- --------------------------------------------------------
 
@@ -256,14 +285,14 @@ CREATE TABLE `remaining_deck` (
 --
 
 INSERT INTO `remaining_deck` (`card_id`, `card_symbol`, `card_color`, `card_code`) VALUES
+(1, '0', 'R', '0R'),
 (2, '1', 'R', '1R'),
-(3, '1', 'R', '1R'),
 (4, '2', 'R', '2R'),
 (5, '2', 'R', '2R'),
 (6, '3', 'R', '3R'),
+(7, '3', 'R', '3R'),
 (8, '4', 'R', '4R'),
 (9, '4', 'R', '4R'),
-(10, '5', 'R', '5R'),
 (11, '5', 'R', '5R'),
 (12, '6', 'R', '6R'),
 (13, '6', 'R', '6R'),
@@ -275,50 +304,52 @@ INSERT INTO `remaining_deck` (`card_id`, `card_symbol`, `card_color`, `card_code
 (19, '9', 'R', '9R'),
 (20, '+2', 'R', '+2R'),
 (21, '+2', 'R', '+2R'),
+(22, 'R', 'R', 'RR'),
 (23, 'R', 'R', 'RR'),
 (24, 'S', 'R', 'SR'),
 (25, 'S', 'R', 'SR'),
 (26, '0', 'Y', '0Y'),
 (27, '1', 'Y', '1Y'),
 (28, '1', 'Y', '1Y'),
-(29, '2', 'Y', '2Y'),
 (30, '2', 'Y', '2Y'),
+(31, '3', 'Y', '3Y'),
 (32, '3', 'Y', '3Y'),
 (33, '4', 'Y', '4Y'),
 (34, '4', 'Y', '4Y'),
 (35, '5', 'Y', '5Y'),
+(36, '5', 'Y', '5Y'),
 (37, '6', 'Y', '6Y'),
 (38, '6', 'Y', '6Y'),
-(39, '7', 'Y', '7Y'),
 (40, '7', 'Y', '7Y'),
 (41, '8', 'Y', '8Y'),
-(42, '8', 'Y', '8Y'),
 (43, '9', 'Y', '9Y'),
-(44, '9', 'Y', '9Y'),
+(45, '+2', 'Y', '+2Y'),
 (46, '+2', 'Y', '+2Y'),
 (47, 'R', 'Y', 'RY'),
+(48, 'R', 'Y', 'RY'),
 (49, 'S', 'Y', 'SY'),
 (50, 'S', 'Y', 'SY'),
 (51, '0', 'B', '0B'),
 (52, '1', 'B', '1B'),
 (53, '1', 'B', '1B'),
 (54, '2', 'B', '2B'),
-(55, '2', 'B', '2B'),
 (56, '3', 'B', '3B'),
 (57, '3', 'B', '3B'),
 (58, '4', 'B', '4B'),
 (59, '4', 'B', '4B'),
-(60, '5', 'B', '5B'),
 (61, '5', 'B', '5B'),
 (62, '6', 'B', '6B'),
 (63, '6', 'B', '6B'),
+(64, '7', 'B', '7B'),
 (65, '7', 'B', '7B'),
 (66, '8', 'B', '8B'),
+(67, '8', 'B', '8B'),
 (68, '9', 'B', '9B'),
+(69, '9', 'B', '9B'),
 (70, '+2', 'B', '+2B'),
 (71, '+2', 'B', '+2B'),
 (72, 'R', 'B', 'RB'),
-(73, 'R', 'B', 'RB'),
+(74, 'S', 'B', 'SB'),
 (75, 'S', 'B', 'SB'),
 (76, '0', 'G', '0G'),
 (77, '1', 'G', '1G'),
@@ -326,19 +357,18 @@ INSERT INTO `remaining_deck` (`card_id`, `card_symbol`, `card_color`, `card_code
 (79, '2', 'G', '2G'),
 (80, '2', 'G', '2G'),
 (81, '3', 'G', '3G'),
-(82, '3', 'G', '3G'),
 (83, '4', 'G', '4G'),
 (84, '4', 'G', '4G'),
 (85, '5', 'G', '5G'),
 (86, '5', 'G', '5G'),
 (87, '6', 'G', '6G'),
-(88, '6', 'G', '6G'),
 (89, '7', 'G', '7G'),
 (90, '7', 'G', '7G'),
 (91, '8', 'G', '8G'),
 (92, '8', 'G', '8G'),
 (93, '9', 'G', '9G'),
 (94, '9', 'G', '9G'),
+(95, '+2', 'G', '+2G'),
 (96, '+2', 'G', '+2G'),
 (97, 'R', 'G', 'RG'),
 (99, 'S', 'G', 'SG'),
@@ -346,10 +376,9 @@ INSERT INTO `remaining_deck` (`card_id`, `card_symbol`, `card_color`, `card_code
 (101, '+4', 'W', '4W'),
 (102, '+4', 'W', '4W'),
 (103, '+4', 'W', '4W'),
+(104, '+4', 'W', '4W'),
 (105, 'N', 'W', 'NW'),
-(106, 'N', 'W', 'NW'),
-(107, 'N', 'W', 'NW'),
-(108, 'N', 'W', 'NW');
+(107, 'N', 'W', 'NW');
 
 -- --------------------------------------------------------
 
@@ -406,7 +435,7 @@ ALTER TABLE `table_deck`
 -- AUTO_INCREMENT για πίνακα `table_deck`
 --
 ALTER TABLE `table_deck`
-  MODIFY `table_id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `table_id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Περιορισμοί για άχρηστους πίνακες
