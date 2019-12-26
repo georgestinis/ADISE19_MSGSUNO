1350419918dbcacf99bded0e3ec7342c12fb573d
George Stinis <44341752+georgestinis@users.noreply.github.com>
2019-12-26 13:15:50 +0200

    Update dbconnect.php

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -6,7 +6,11 @@ require_once "local_config.php";
 $user=USERNAME;
 $pass=DB_PWD;
 
-$mysqli= new mysqli($host, $user, $pass, $db);
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it174890/mysql/run/mysql.sock');
+} else {
+    $mysqli = new mysqli($host, $user, $pass, $db);
+}
 
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . 
